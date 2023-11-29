34aee7a1d21739a9a0c486b766e896c37992894f
Gabriellm-dev <gabriellm.dev@gmail.com>
2023-11-29 16:07:57 -0300

    findAll implement

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,11 +18,19 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("\n=== TEST 1: seller findById ===");
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
         List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
 
         for (Seller obj: list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+
+        for (Seller obj: list){
+            System.out.println(obj);
+        }
+
     }
 }
